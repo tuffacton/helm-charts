f16ba2c27dbf26573995699c654d261234ebccdf
Nic Acton <nicolas.acton@harness.io>
2024-11-27 15:37:30 -0500

    clean up variable file

--- eks-terraform/variables.tf
+++ eks-terraform/variables.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 variable "region" {
-  description = "AWS region"
+  description = "AWS region, can be either commercial (e.g. us-east-1) or Govcloud (e.g. us-gov-east-1)"
   type        = string
   default     = "us-east-1"
 }
